930764d799801a6be1bd744667285f5d6b9d2636
raydtang <rayd_tang@yahoo.com>
2015-08-03 18:50:50 +0800

    Update rem.js

--- rem.js
+++ rem.js
@@ -9,9 +9,9 @@ function refreshWebview_cb()
   AndroidFunction.refreshWebview();
 }
 
-function rem_run(int cmd)
+function rem_run(run_code)
 {
-  switch(cmd) {
+  switch(run_code) {
     case 0:
       AndroidFunction.remoteCMD("date");
       break;
