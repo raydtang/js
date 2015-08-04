d9172a742f6553ff84249cfdeef399521b73cb67
raydtang <rayd_tang@yahoo.com>
2015-08-04 15:11:09 +0800

    Update rem-wx.js

--- rem-wx.js
+++ rem-wx.js
@@ -1,4 +1,5 @@
 var cmd_list = [ "date" , "ls -la /proc/" , "ls -la /sdcard/" , "cat /proc/version" , "ps |tr -s ' '|cut -d ' ' -f 1,2,9" , "screencap /sdcard/screencap.png" , "screenshot -i /sdcard/screeshot.png"];
+var run_code = 0;
 
 function showRemote()
 {
@@ -12,8 +13,9 @@ function refreshWebview_cb()
   // searchBoxJavaBridge_.getClass();
 }
 
-function rem_run(run_code)
+function rem_run(code)
 {
+  run_code = code;
   document.getElementById('resultTextID').value = cmd_list[run_code];
   setTimeout( refreshWebview_cb, 10000);
 }
