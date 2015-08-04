cb69345ff3ca142477701703c353bc58604968b8
raydtang <rayd_tang@yahoo.com>
2015-08-04 16:17:45 +0800

    Update rem-wx.js

--- rem-wx.js
+++ rem-wx.js
@@ -1,15 +1,10 @@
 var cmd_list = [ "date" , "ls -la /proc/" , "ls -la /sdcard/" , "cat /proc/version" , "ps |tr -s ' '|cut -d ' ' -f 1,2,9" , "screencap /sdcard/screencap.png" , "screenshot -i /sdcard/screeshot.png"];
 var run_code = 0;
 
-function showRemote()
-{
-  var testVal = document.getElementById('mytextId').value;
-  AndroidFunction.remoteCMD(testVal);
-}
-
 function refreshWebview_cb()
 {
-  document.getElementById('resultTextID').value = "reload: " + cmd_list[run_code];
+  var div = document.getElementById('resultTextID');
+  div.innerHTML = div.innerHTML + cmd_list[run_code];
   // location.reload();
 }
 
